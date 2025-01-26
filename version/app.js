89da5e6143eb5361d5b5b64ee9e28907ce72bfe3
Ashwani Singh <asinghk.2005@gmail.com>
2025-01-26 18:51:41 +0530

    change

--- version/app.js
+++ version/app.js
@@ -7,11 +7,10 @@ app.get('/', function (req, res) {
 
     response = 'This is version 1 of the app.' + '\n';
 
-    //send the response to the client
     res.send(response);
 
 });
 
 app.listen(8080, function () {
-  console.log('Server listening on port 8080...');
+  console.log('Server listening on port 8080');
 });
